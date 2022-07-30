79cce8465bbd566181c53adda34c50214170e0ff
Gekka58 <He588@yandex.ru>
2022-07-31 01:01:18 +0300

    1

--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -5,9 +5,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Ocean1 {
-
     @Autowired
     private Island2 island;
+
 
     @Override
     public String toString() {
